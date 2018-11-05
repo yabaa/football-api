fdbfa7457fffce252fb78f639cbc1113d702efb9
Yahya Baassou <yahyabaassou@gmail.com>
2018-11-05 18:05:07 +0100

    initial commit

---
+++ src/main/java/com/github/yahyaba/footballapi/footballapi/FootballApiApplication.java
@@ -0,0 +1,12 @@
+package com.github.yahyaba.footballapi.footballapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballApiApplication.class, args);
+	}
+}
